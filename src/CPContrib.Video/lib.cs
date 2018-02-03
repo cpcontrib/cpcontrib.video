c711d82e1a26110ca40ab4f0a1400f2d078ae5be
Eric Newton <ericnewton76@gmail.com>
2018-02-03 16:10:47 -0500

    initial commit

---
+++ src/CPContrib.Video/lib.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CrownPeak.CMSAPI;
+using CrownPeak.CMSAPI.Services;
+/* Some Namespaces are not allowed. */
+namespace CPContrib.Video
+{
+	using System.Text.RegularExpressions;
+	using CPContrib.Video.Core;
+
+	public class VideoLinkParser
+	{
+		public VideoLinkParser()
+		{
+		}
+
+		public VideoLinkInfo ParseLink(string value)
+		{
+			//simple parser for now
+			Match m;
+			VideoLinkInfo info;
+
+			if((m = S_YoutubeLink.Match(value)).Success)
+			{
+				info = new VideoLinkInfo();
+				info.Company = Constants.YouTubeCompany;
+				return info;
+			}
+
+			if((m = S_BrightcoveLink.Match(value)).Success)
+			{
+				VideoLinkInfo i = new VideoLinkInfo();
+				i.Company = Constants.YouTubeCompany;
+				return i;
+			}
+
+			throw new FormatException("Failed to parse the provider link");
+		}
+
+		private static Regex S_YoutubeLink = new Regex(@"http(s)?://(youtu.be|youtube.com)/");
+		private static Regex S_BrightcoveLink = new Regex(@"http(s)?://(vid.brightcove.com)/");
+
+	}
+
+
+	
+
+	public class VideoLinkInfo
+	{
+		public IVideoHtmlFactory ProviderFactory { get; set; }
+		public string Company { get; set; }
+		public string VideoId { get; set; }
+	}
+
+	public static class Constants
+	{
+		public const string YouTubeCompany = "YouTube";
+		public const string YouTubeEmbed = @"<iframe width=""{width}"" height=""{height}"" src=""https://www.youtube.com/embed/F_kXj7PSCac?rel=0"" frameborder=""0"" allow=""autoplay; encrypted-media"" allowfullscreen></iframe>";
+		public const string BrightcoveCompany = "Brightcove";
+
+	}
+
+	public class BrightcoveHtml : IVideoHtml
+	{
+
+		public string GetEmbed(VideoLinkInfo videoInfo, Size size, IDictionary<string,string> embedoptions = null)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+
+	public class YoutubeHtml : IVideoHtml
+	{
+		public string GetEmbed(VideoLinkInfo videoInfo, Size size, IDictionary<string,string> embedoptions = null)
+		{
+			StringBuilder sb = new StringBuilder(Constants.YouTubeEmbed);
+
+			return sb.ToString();
+		}
+	}
+}
+namespace CPContrib.Video.Core
+{
+	public interface IVideoHtmlFactory
+	{
+		IVideoHtml CreateProvider();
+	}
+	public class SimpleVideoHtmlProviderFactory
+	{
+		public SimpleVideoHtmlProviderFactory(string company)
+		{
+			this._Company = company;
+		}
+		private string _Company;
+
+		public IVideoHtml CreateProvider()
+		{
+			return CreateProvider(this._Company);
+		}
+
+		public IVideoHtml CreateProvider(string company)
+		{
+			switch(company)
+			{
+				case "Brightcove":
+					return new BrightcoveHtml();
+				case "YouTube":
+					return new YoutubeHtml();
+
+			}
+			throw new NotImplementedException(string.Format("Company '{0}' not implemented yet."));
+		}
+	}
+
+	public interface IVideoHtml
+	{
+		string GetEmbed(VideoLinkInfo videoInfo, Size size, IDictionary<string,string> embedoptions = null);
+
+	}
+
+
+}
---
+++ src/CPContrib.Video_Tests/VideoLinkParser_Tests.cs
@@ -0,0 +1,45 @@
+﻿using CPContrib.Video;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPContrib.Video_Tests
+{
+	[TestFixture]
+	public class VideoLinkParser_Tests
+	{
+		[Test]
+		public void FormatException_when_not_recognized()
+		{
+			
+			Assert.Throws<FormatException>(() => {
+				var parser = new VideoLinkParser();
+
+				parser.ParseLink("");
+
+			});
+			
+		}
+
+		[Test]
+		[TestCase("http://youtube.com/randomid")]
+		[TestCase("http://youtu.be/randomid")]
+		[TestCase("https://youtu.be/randomid")]
+		[TestCase("https://youtu.be/randomid")]
+		public void YouTube_recognized(string value)
+		{
+			//arrange
+			var parser = new VideoLinkParser();
+
+			//act
+			var actual = parser.ParseLink(value);
+
+			//assert
+			Assert.That(actual.Company, Is.EqualTo(Constants.YouTubeCompany));
+		}
+
+	}
+}
